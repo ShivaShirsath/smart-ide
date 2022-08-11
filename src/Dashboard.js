2eb219c35e55a4a00d64c1fc634aff076fe5522a
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2022-08-12 01:54:37 +0530

    deploy 😊 firebase added and pwa also 😅

---
+++ src/Dashboard.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { useAuth } from "./firebase/AuthContext"
+import { useHistory } from "react-router-dom"
+import Editor from "./activities/Editor";
+import useLocalStorage from "./hooks/useLocalStorage";
+
+function Dashboard() {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+        </html>
+        `);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+    } catch {
+      setError("Failed to log out")
+    }
+  }
+  
+  return (
+    <stator>
+    {error && <span>{error}</span>}
+      <iframe
+        srcDoc={srcDoc}
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+      <rotor>
+      { currentUser.displayName }
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      <logout onClick={handleLogout}>
+        ⎋
+      </logout>
+      </rotor>
+    </stator>
+  );
+}
+
+export default Dashboard;
---
+++ src/firebase/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+import { useAuth } from "./AuthContext"
+import { useHistory } from "react-router-dom"
+
+export default function Login() {
+  const { login } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setError("")
+      setLoading(true)
+      await login()
+      history.push("/")
+    } catch {
+      setError("Failed to log in")
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <h2 align="center">Log In</h2>
+      {error && <span>{error}</span>}
+      <button disabled={loading} type="submit" >
+        Login
+      </button>
+    </form>
+  )
+}
