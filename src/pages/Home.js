8b3f67b80d40c71d2433ce1566a3d85bcda4a075
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-12-04 19:10:18 +0530

    formated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
-import { v4 as uuidV4 } from 'uuid';
-import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [roomId, setRoomId] = useState('');
-    const [username, setUsername] = useState('');
-    const createNewRoom = (e) => {
-        e.preventDefault();
-        const id = uuidV4();
-        setRoomId(id);
-        toast.success('Created a new room');
-    };
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
 
-    const joinRoom = () => {
-        if (!roomId || !username) {
-            toast.error('ROOM ID & username is required');
-            return;
-        }
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("ROOM ID & username is required");
+      return;
+    }
 
-        // Redirect
-        navigate(`/editor/${roomId}`, {
-            state: {
-                username,
-            },
-        });
-    };
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
 
-    const handleInputEnter = (e) => {
-        if (e.code === 'Enter') {
-            joinRoom();
-        }
-    };
-    return (
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
+  return (
     <div className="formWrapper">
       <div className="inputGroup">
         <input
@@ -81,7 +81,7 @@ const Home = () => {
         </h4>
       </footer>
     </div>
-    );
+  );
 };
 
 export default Home;
