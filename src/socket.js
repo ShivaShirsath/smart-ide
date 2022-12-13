951a80954e05650dc6c95f4a56c70c21d235dfff
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-12-14 02:21:47 +0530

    deploy

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const initSocket = async () => {
-	return io("https://smart-ide-server.onrender.com", {
+	return io("https://smart-ide.onrender.com", {
 		"force new connection": true,
 		reconnectionAttempt: "Infinity",
 		timeout: 10000,
