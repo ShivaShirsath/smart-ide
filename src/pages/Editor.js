8c469692d0a7de38ced80483572cc6c93f920c9a
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2023-04-15 11:31:02 +0530

    changes

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -282,7 +282,7 @@ const Editors = ({ socketRef, roomId, onCodeChange }) => {
       </form>
       <Editor
         id="editor"
-        height="65%"
+        height="70%"
         width="100%"
         onMount={handleEditorDidMount}
         path={file.name}
